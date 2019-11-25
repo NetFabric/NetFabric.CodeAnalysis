e2609415fc7de7070eea25a9ef6aaa31555b8e0e
Antao Almada <antao.almada@gmail.com>
2019-11-25 23:08:58 +0000

    Add first unit tests

---
+++ NetFabric.RoslynHelpers.UnitTests/ITypeSymbolExtensionsTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.CodeAnalysis;
+using NetFabric.Assertive;
+using System.Linq;
+using Xunit;
+
+namespace NetFabric.RoslynHelpers.UnitTests
+{
+    public partial class ITypeSymbolExtensionsTests
+    {
+        [Fact]
+        public void GetMethod_With_NoParameters_Should_Return()
+        {
+            // Arrange
+            var code = @"
+            using System;
+            public class ExampleClass 
+            {
+                readonly string message;
+
+                public ExampleClass()
+                {
+                    message = ""Hello World"";
+                }
+
+                public string GetMessage() 
+                    => message;
+            }";
+            var compilation = Utils.Compile(code);
+            var exampleClass = compilation.GetSymbolsWithName("ExampleClass").First() as ITypeSymbol;
+
+            // Act
+            var result = exampleClass.GetPublicMethod("GetMessage");
+
+            // Assert   
+            result.Must()
+                .BeNotNull()
+                .EvaluatesTrue(method => 
+                    method.Name == "GetMessage" &&
+                    method.Parameters.Length == 0);
+        }
+
+        [Fact]
+        public void GetMethod_With_Parameters_Should_Return()
+        {
+            // Arrange
+            var code = @"
+            using System;
+            public class ExampleClass 
+            {
+                readonly string message;
+
+                public ExampleClass()
+                {
+                    message = ""Hello World"";
+                }
+
+                public string GetMessage() 
+                    => message;
+
+                public string GetMessage(int prefix) 
+                    => $""{prefix} message"";
+
+                public string GetMessage(string prefix) 
+                    => $""{prefix} message"";
+            }";
+            var compilation = Utils.Compile(code);
+            var exampleClass = compilation.GetSymbolsWithName("ExampleClass").First() as ITypeSymbol;
+
+            // Act
+            var result = exampleClass.GetPublicMethod("GetMessage", typeof(string));
+
+            // Assert   
+            result.Must()
+                .BeNotNull()
+                .EvaluatesTrue(method =>
+                    method.Name == "GetMessage" &&
+                    method.Parameters.Length == 1 &&
+                    method.Parameters[0].Type.Name == "String");
+        }
+    }
+}
---
+++ NetFabric.RoslynHelpers.UnitTests/Utils.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace NetFabric.RoslynHelpers.UnitTests
+{
+    public static class Utils
+    {
+        public static CSharpCompilation Compile(string text)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(text);
+            return CSharpCompilation.Create(
+                "assemblyName",
+                new[] { syntaxTree },
+                new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+        }
+    }
+}
