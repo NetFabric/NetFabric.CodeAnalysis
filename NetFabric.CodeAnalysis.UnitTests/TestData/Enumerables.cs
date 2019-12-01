60223b5af68f984b47a67e5957638a8b0b38b903
Antão Almada <antao.almada@gmail.com>
2019-12-01 15:16:25 +0000

    Validate test data

--- NetFabric.CodeAnalysis.UnitTests/TestData/Enumerables.cs
+++ NetFabric.CodeAnalysis.UnitTests/TestData/Enumerables.cs
@@ -10,58 +10,66 @@ namespace NetFabric.CodeAnalysis.TestData
     {
     }
 
+    public readonly struct MissingCurrentEnumerable
+    {
+        public MissingCurrentEnumerator GetEnumerator() => new MissingCurrentEnumerator();
+        public MissingCurrentEnumerator GetAsyncEnumerator() => new MissingCurrentEnumerator();
+    }
+
+    public readonly struct MissingMoveNextEnumerable<T>
+    {
+        public MissingMoveNextEnumerator<T> GetEnumerator() => new MissingMoveNextEnumerator<T>();
+        public MissingMoveNextEnumerator<T> GetAsyncEnumerator() => new MissingMoveNextEnumerator<T>();
+    }
+
     public readonly struct Enumerable<T>
     {
-        public readonly Enumerable<T> GetEnumerator() => this;
-
-        public readonly T Current => default;
-
-        public bool MoveNext() => false;
+        public readonly Enumerator<T> GetEnumerator() => new Enumerator<T>();
     }
 
     public readonly struct AsyncEnumerable<T>
     {
-        public readonly AsyncEnumerable<T> GetAsyncEnumerator() => this;
-
-        public readonly T Current => default;
-
-        public ValueTask<bool> MoveNextAsync() => new ValueTask<bool>(Task.FromResult(false));
+        public readonly AsyncEnumerator<T> GetAsyncEnumerator() => new AsyncEnumerator<T>();
     }
 
-    public class ExplicitEnumerable<T> : IEnumerable<T>, IEnumerator<T>
+    public class ExplicitEnumerable<T> : IEnumerable<T>
     {
-        IEnumerator<T> IEnumerable<T>.GetEnumerator() => this;
-        IEnumerator IEnumerable.GetEnumerator() => this;
-
-        T IEnumerator<T>.Current => default;
-        object IEnumerator.Current => default;
-
-        bool IEnumerator.MoveNext() => false;
-
-        void IEnumerator.Reset() { }
-
-        void IDisposable.Dispose() { }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => new ExplicitEnumerator<T>();
+        IEnumerator IEnumerable.GetEnumerator() => new ExplicitEnumerator<T>();
     }
 
-    public class ExplicitEnumerable : IEnumerable, IEnumerator
+    public class ExplicitEnumerable : IEnumerable
     {
-        IEnumerator IEnumerable.GetEnumerator() => this;
-
-        object IEnumerator.Current => default;
-
-        bool IEnumerator.MoveNext() => false;
-
-        void IEnumerator.Reset() { }
+        IEnumerator IEnumerable.GetEnumerator() => new ExplicitEnumerator();
     }
 
-    public class ExplicitAsyncEnumerable<T> : IAsyncEnumerable<T>, IAsyncEnumerator<T>
+    public class ExplicitAsyncEnumerable<T> : IAsyncEnumerable<T>
     {
-        IAsyncEnumerator<T> IAsyncEnumerable<T>.GetAsyncEnumerator(CancellationToken _) => this;
+        IAsyncEnumerator<T> IAsyncEnumerable<T>.GetAsyncEnumerator(CancellationToken _) => new ExplicitAsyncEnumerator<T>();
+    }
 
-        T IAsyncEnumerator<T>.Current => default;
+    static class ValidateEnumerables
+    {
+        public static async Task InvalidEnumerables()
+        {
+            //foreach (var _ in new MissingGetEnumeratorEnumerable()) { }
+            //foreach (var _ in new MissingCurrentEnumerable()) { }
+            //foreach (var _ in new MissingMoveNextEnumerable<int>()) { }
 
-        ValueTask<bool> IAsyncEnumerator<T>.MoveNextAsync() => new ValueTask<bool>(Task.FromResult(false));
+            //await foreach (var _ in new MissingGetEnumeratorEnumerable()) { }
+            //await foreach (var _ in new MissingCurrentEnumerable()) { }
+            //await foreach (var _ in new MissingMoveNextEnumerable<int>()) { }
+        }
 
-        ValueTask IAsyncDisposable.DisposeAsync() => new ValueTask();
+        public static async Task ValidEnumerables()
+        {
+            foreach (var _ in new Enumerable<int>()) { }
+            foreach (var _ in new ExplicitEnumerable<int>()) { }
+            foreach (var _ in new RangeEnumerable()) { }
+
+            await foreach (var _ in new AsyncEnumerable<int>()) { }
+            await foreach (var _ in new ExplicitAsyncEnumerable<int>()) { }
+            await foreach (var _ in new RangeAsyncEnumerable()) { }
+        }
     }
 }
