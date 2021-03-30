1c62b2344682c7d08351ec305c2ad77b9a9db08f
Antão Almada <antao.almada@gmail.com>
2021-03-30 19:39:23 +0100

    Optimize code generated by ForEach in case of an array (#8)

--- NetFabric.Reflection/Expressions/ExpressionEx.ForEach.cs
+++ NetFabric.Reflection/Expressions/ExpressionEx.ForEach.cs
@@ -30,51 +30,75 @@ namespace NetFabric.Expressions
 
         public static Expression ForEach(EnumerableInfo enumerableInfo, Expression enumerable, Func<Expression, Expression> body)
         {
-            var enumeratorType = enumerableInfo.GetEnumerator.ReturnType;
-            var enumeratorInfo = enumerableInfo.EnumeratorInfo;
-            var enumerator = Variable(enumeratorType, "enumerator");
-            return Block(
-                new[] { enumerator },
-                Assign(enumerator, Call(enumerable, enumerableInfo.GetEnumerator)),
-                enumeratorInfo switch
-                {
-                    { Dispose: not null } => Disposable(enumeratorInfo, enumerator, body),
-                    _ => enumeratorType switch
-                    {
-                        { IsValueType: true } => NonDisposableValueType(enumeratorInfo, enumerator, body),
-                        _ => NonDisposableReferenceType(enumeratorInfo, enumerator, body)
-                    }
-                }
-            );
-                
-            static Expression Disposable(EnumeratorInfo enumeratorInfo, Expression enumerator, Func<Expression, Expression> body)
-                => Using(enumerator,
-                        Enumeration(enumeratorInfo, enumerator, body)
-                    );
-                
-            static Expression NonDisposableValueType(EnumeratorInfo enumeratorInfo, Expression enumerator, Func<Expression, Expression> body)
-                => Enumeration(enumeratorInfo, enumerator, body);
-                
-            static Expression NonDisposableReferenceType(EnumeratorInfo enumeratorInfo, ParameterExpression  enumerator, Func<Expression, Expression> body)
+            return enumerable.Type switch
             {
-                var disposable = Variable(typeof(IDisposable), "disposable");
-                return TryFinally(
-                    Enumeration(enumeratorInfo, enumerator, body),
-                    Block(
-                        new [] { disposable, enumerator },
-                        Assign(disposable, TypeAs(enumerator, typeof(IDisposable))),
-                        IfThen(
-                            NotEqual(disposable, Constant(null)),
-                            Call(disposable, Reflection.TypeExtensions.DisposeInfo)
+                { IsArray: true } => HandleArray(enumerable, body),
+                _ => HandleEnumerable(enumerableInfo, enumerable, body)
+            };
+
+            static Expression HandleArray(Expression array, Func<Expression, Expression> body)
+            {
+                var indexParameter = Parameter(typeof(int), "index");
+                return Block(
+                    new[] { indexParameter },
+                    Assign(indexParameter, Constant(0)),
+                    While(
+                        LessThan(indexParameter, Property(array, typeof(Array).GetPublicInstanceDeclaredOnlyProperty(nameof(Array.Length))!)),
+                        Block(
+                            body(ArrayIndex(array, indexParameter)),
+                            PostIncrementAssign(indexParameter)
                         )
                     )
                 );
             }
 
-            static Expression Enumeration(EnumeratorInfo enumeratorInfo, Expression enumerator, Func<Expression, Expression> body)
-                => While(Call(enumerator, enumeratorInfo.MoveNext),
-                    body(Property(enumerator, enumeratorInfo.Current))
-                );
+            static Expression HandleEnumerable(EnumerableInfo enumerableInfo, Expression enumerable, Func<Expression, Expression> body)
+            {
+                var enumeratorType = enumerableInfo.GetEnumerator.ReturnType;
+                var enumeratorInfo = enumerableInfo.EnumeratorInfo;
+                var enumerator = Variable(enumeratorType, "enumerator");
+                return Block(
+                    new[] {enumerator},
+                    Assign(enumerator, Call(enumerable, enumerableInfo.GetEnumerator)),
+                    enumeratorInfo switch
+                    {
+                        {Dispose: not null} => Disposable(enumeratorInfo, enumerator, body),
+                        _ => enumeratorType switch
+                        {
+                            {IsValueType: true} => NonDisposableValueType(enumeratorInfo, enumerator, body),
+                            _ => NonDisposableReferenceType(enumeratorInfo, enumerator, body)
+                        }
+                    });
+
+                static Expression Disposable(EnumeratorInfo enumeratorInfo, Expression enumerator, Func<Expression, Expression> body)
+                    => Using(enumerator, 
+                        EnumerationLoop(enumeratorInfo, enumerator, body)
+                    );
+
+                static Expression NonDisposableValueType(EnumeratorInfo enumeratorInfo, Expression enumerator, Func<Expression, Expression> body)
+                    => EnumerationLoop(enumeratorInfo, enumerator, body);
+
+                static Expression NonDisposableReferenceType(EnumeratorInfo enumeratorInfo, ParameterExpression enumerator, Func<Expression, Expression> body)
+                {
+                    var disposable = Variable(typeof(IDisposable), "disposable");
+                    return TryFinally(
+                        EnumerationLoop(enumeratorInfo, enumerator, body),
+                        Block(
+                            new[] {disposable, enumerator},
+                            Assign(disposable, TypeAs(enumerator, typeof(IDisposable))),
+                            IfThen(
+                                NotEqual(disposable, Constant(null)),
+                                Call(disposable, Reflection.TypeExtensions.DisposeInfo)
+                            )
+                        )
+                    );
+                }
+
+                static Expression EnumerationLoop(EnumeratorInfo enumeratorInfo, Expression enumerator, Func<Expression, Expression> body)
+                    => While(Call(enumerator, enumeratorInfo.MoveNext),
+                        body(Property(enumerator, enumeratorInfo.Current))
+                    );
+            }
         }
     }
 }
